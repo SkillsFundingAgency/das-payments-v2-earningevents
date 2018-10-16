bcf97a7f56aecd2bbb9f49ece64eb002e00930cd
abroodie <abroodie@outlook.com>
2018-10-16 13:26:41 +0100

    Added partially implemented acceptance test for earning events

---
+++ src/SFA.DAS.Payments.EarningEvents.Messages.Internal/Commands/ProcessLearnerCommand.cs
@@ -0,0 +1,13 @@
+﻿using ESFA.DC.ILR.FundingService.FM36.FundingOutput.Model.Output;
+using SFA.DAS.Payments.Messages.Core.Commands;
+
+namespace SFA.DAS.Payments.EarningEvents.Messages.Internal.Commands
+{
+    public class ProcessLearnerCommand: PaymentsCommand
+    {
+        public long Ukprn { get; set; }
+        public string CollectionYear { get; set; }
+        public int CollectionPeriod { get; set; }
+        public FM36Learner Learner { get; set; }
+    }
+}
