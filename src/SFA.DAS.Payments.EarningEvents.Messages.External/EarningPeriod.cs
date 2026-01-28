59246537f162577ab3d465658bb947ef036a8570
David Read <david2.read@education.gov.uk>
2026-01-28 13:08:04 +0000

    PV2-3840 add new external messages Nuget package for short courses

---
+++ src/SFA.DAS.Payments.EarningEvents.Messages.External/EarningPeriod.cs
@@ -0,0 +1,11 @@
+﻿
+namespace SFA.DAS.Payments.EarningEvents.Messages.External
+{
+    public class EarningPeriod
+    {
+        public int DeliveryPeriod { get; set; }
+        public EarningType EarningType { get; set; }
+        public decimal Amount { get; set; }
+        public Employer Employer { get; set; }
+    }
+}
