ecb50d4db280816013d9fc15bd2e7ef95ab3c395
David Read <david2.read@education.gov.uk>
2026-02-02 11:50:45 +0000

    PV2-3830 add empty application project and dummy test to test pipeline

---
+++ src/SFA.DAS.Payments.EarningEvents.EarningsBridge.Application/Validators/CalculateGSLPaymentsValidator.cs
@@ -0,0 +1,12 @@
+﻿using SFA.DAS.Payments.EarningEvents.Messages.External.Commands;
+
+namespace SFA.DAS.Payments.EarningEvents.EarningsBridge.Application.Validators
+{
+    public static class CalculateGSLPaymentsValidator
+    {
+        public static bool Validate(CalculateGSLPayments command)
+        {
+            return true;
+        }
+    }
+}
---
+++ src/SFA.DAS.Payments.EarningEvents.EarningsBridge.UnitTests/CalculateGSLPaymentsValidatorTests.cs
@@ -0,0 +1,15 @@
+﻿using SFA.DAS.Payments.EarningEvents.EarningsBridge.Application.Validators;
+using SFA.DAS.Payments.EarningEvents.Messages.External.Commands;
+
+namespace SFA.DAS.Payments.EarningEvents.EarningsBridge.Application.UnitTests
+{
+    [TestFixture]
+    public class CalculateGSLPaymentsValidatorTests
+    {
+        [Test]
+        public void Validate_returns_true()
+        {
+            var result = CalculateGSLPaymentsValidator.Validate(new CalculateGSLPayments());
+        }
+    }
+}
