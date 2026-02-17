a5986ae43ac0a3b58a28df897dac5eb184ac7028
David Read <david2.read@education.gov.uk>
2026-02-17 21:14:30 +0000

    PV2-3888 add configuration

---
+++ src/SFA.DAS.Payments.EarningEvents.EarningsBridge.Application/Infrastructure/Configuration/IEarningsBridgeConfiguration.cs
@@ -0,0 +1,11 @@
+﻿namespace SFA.DAS.Payments.EarningEvents.EarningsBridge.Application.Infrastructure.Configuration
+{
+    public interface IEarningsBridgeConfiguration
+    {
+        string PaymentsConnectionString { get; set; }
+        string DASServiceBusConnectionString { get; set; }
+        string DASServiceBusQueueName { get; set; }
+        string PaymentsServiceBusConnectionString { get; set; }
+        string CollectionPeriodApiBaseAddress { get; set; }
+    }
+}
