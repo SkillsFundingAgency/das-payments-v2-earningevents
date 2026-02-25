401cc16ab73c3827bd7d9acc02436e759f1834df
David Read <david2.read@education.gov.uk>
2026-02-25 09:26:55 +0000

    PV2-3824 fix variable naming

--- src/SFA.DAS.Payments.EarningEvents.DASEarningsBridge.Function/DASEarningsReceiver.cs
+++ src/SFA.DAS.Payments.EarningEvents.DASEarningsBridge.Function/DASEarningsReceiver.cs
@@ -11,12 +11,12 @@ namespace SFA.DAS.Payments.EarningEvents.EarningsBridge.Function
     public class DASEarningsReceiver
     {
         private readonly ILogger<DASEarningsReceiver> _logger;
-        private readonly IGSLCalculatePaymentsHandler _gSLCalculatePaymentsHandler;
+        private readonly IGSLCalculatePaymentsHandler _gslCalculatePaymentsHandler;
 
-        public DASEarningsReceiver(ILogger<DASEarningsReceiver> logger, IGSLCalculatePaymentsHandler gSLCalculatePaymentsHandler)
+        public DASEarningsReceiver(ILogger<DASEarningsReceiver> logger, IGSLCalculatePaymentsHandler gslCalculatePaymentsHandler)
         {
             _logger = logger;
-            _gSLCalculatePaymentsHandler = gSLCalculatePaymentsHandler;
+            _gslCalculatePaymentsHandler = gslCalculatePaymentsHandler;
         }
 
         [Function(nameof(DASEarningsReceiver))]
@@ -30,7 +30,7 @@ namespace SFA.DAS.Payments.EarningEvents.EarningsBridge.Function
             _logger.LogInformation("Message Content-Type: {contentType}", message.ContentType);
 
             var growthAndSkillsPaymentsMessage = message.Body.ToObjectFromJson<CalculateGrowthAndSkillsPayments>();
-            await _gSLCalculatePaymentsHandler.HandleGslCalculatePaymentsMessage(growthAndSkillsPaymentsMessage);
+            await _gslCalculatePaymentsHandler.HandleGslCalculatePaymentsMessage(growthAndSkillsPaymentsMessage);
 
             await messageActions.CompleteMessageAsync(message);
         }
