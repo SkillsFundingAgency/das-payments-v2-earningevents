ff348f9d2e30f103afc50f89d1e4a1e4b18fff43
David Read <david2.read@education.gov.uk>
2026-05-14 13:03:53 +0100

    Update UUID comparison tests to benchmark each operation

--- src/SFA.DAS.Payments.EarningEvents.EarningsBridge.Application.UnitTests/UUIDComparisionTests.cs
+++ src/SFA.DAS.Payments.EarningEvents.EarningsBridge.Application.UnitTests/UUIDComparisionTests.cs
@@ -1,5 +1,6 @@
 ﻿
 using System.Data.SqlTypes;
+using System.Diagnostics;
 using UUIDNext;
 using UUIDNext.Tools;
 
@@ -8,10 +9,10 @@ namespace SFA.DAS.Payments.EarningEvents.EarningsBridge.Application.UnitTests
     [TestFixture]
     public class UUIDComparisionTests
     {
-        private const int Iterations = 100;
+        private const int Iterations = 1000000;
 
-        [Test]
-        public void UUID_CompareTo_reliably_compares_two_UUID_v8_values() // fails in first few iterations
+        [Test,Ignore("fails in first few iterations")]
+        public void UUID_CompareTo_reliably_compares_two_UUID_v8_values() 
         {
             for(int i = 1; i <= Iterations; i++)
             {
@@ -25,8 +26,8 @@ namespace SFA.DAS.Payments.EarningEvents.EarningsBridge.Application.UnitTests
             }
         }
 
-        [Test]
-        public void GuidComparer_reliably_compares_two_UUID_v8_values() // fails in first few iterations
+        [Test, Ignore("fails in first few iterations")]
+        public void GuidComparer_reliably_compares_two_UUID_v8_values() 
         {
             var comparer = new GuidComparer();
             for (int i = 1; i <= Iterations; i++)
@@ -42,34 +43,44 @@ namespace SFA.DAS.Payments.EarningEvents.EarningsBridge.Application.UnitTests
         }
 
         [Test]
-        public void Timestamp_comparison_reliably_compares_two_UUID_v8_values() // reliable
+        public void Timestamp_comparison_reliably_compares_two_UUID_v8_values() 
         {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+
             for (int i = 1; i <= Iterations; i++)
             {
                 var firstUUID = Uuid.NewDatabaseFriendly(Database.SqlServer);
-                Thread.Sleep(1);
+                //Thread.Sleep(1);
                 var secondUUID = Uuid.NewDatabaseFriendly(Database.SqlServer);
 
                 var firstEventIdDecodesToTimestamp = UuidDecoder.TryDecodeTimestamp(firstUUID, out var firstEventDateTime);
                 var secondEventIdDecodesToTimestamp = UuidDecoder.TryDecodeTimestamp(secondUUID, out var secondEventDateTime);
                 
-                Assert.That(secondEventDateTime, Is.GreaterThan(firstEventDateTime), "Comparison failed on iteration " + i);
+                Assert.That(secondEventDateTime, Is.GreaterThanOrEqualTo(firstEventDateTime), "Comparison failed on iteration " + i);
             }
+
+            Console.WriteLine($"{Iterations} comparisons completed in {stopwatch.ElapsedMilliseconds} milliseconds");
         }
 
         [Test]
-        public void SqlGuid_reliably_compares_two_SQL_Server_UUID_v8_values() // reliable
+        public void SqlGuid_reliably_compares_two_SQL_Server_UUID_v8_values() 
         {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+
             for (int i = 1; i <= Iterations; i++)
             {
                 var firstUUID = Uuid.NewDatabaseFriendly(Database.SqlServer);
-                Thread.Sleep(1);
+                //Thread.Sleep(1);
                 var secondUUID = Uuid.NewDatabaseFriendly(Database.SqlServer);
 
                 var result = new SqlGuid(secondUUID).CompareTo(new SqlGuid(firstUUID));
 
-                Assert.That(result, Is.GreaterThan(0), "Comparison failed on iteration " + i);
+                Assert.That(result, Is.GreaterThanOrEqualTo(0), "Comparison failed on iteration " + i);
             }
+
+            Console.WriteLine($"{Iterations} comparisons completed in {stopwatch.ElapsedMilliseconds} milliseconds");
         }
     }
 }
