74ac7648a7bb617d6a9e56e4fb96c53adbdf171a
Winzar <33451710+Winzar@users.noreply.github.com>
2026-08-14 13:10:18 +0100

    PV2-4221 Implementing interface used in 4191's ticket

--- src/SFA.DAS.Payments.EarningEvents.EarningsBridge.Application/Mapping/IGSLApprenticeshipsMapper.cs
+++ src/SFA.DAS.Payments.EarningEvents.EarningsBridge.Application/Mapping/IGSLApprenticeshipsMapper.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SFA.DAS.Payments.EarningEvents.Messages.Events;
+using SFA.DAS.Payments.EarningEvents.Messages.External.Commands;
+using SFA.DAS.Payments.Model.Core.Entities;
 
 namespace SFA.DAS.Payments.EarningEvents.EarningsBridge.Application.Mapping
 {
     public interface IGslApprenticeshipsMapper : IGrowthAndSkillsMapper
     {
+        IEnumerable<GSLApprenticeshipEarningsEvent> MapToApprenticeshipEarningEvents(CalculateGrowthAndSkillsPayments source, IEnumerable<CollectionPeriodModel> openCollectionPeriods);
     }
 }
